b9bc3e2bf68523a431a3055d1e3fac907ab23248
John Cayley <John_Cayley@brown.edu>
2017-11-17 14:46:55 -0500

    sync still hard-coded for classes but will now sync per student

--- scripts/Admin/sync_w3d_class.sh
+++ scripts/Admin/sync_w3d_class.sh
@@ -3,12 +3,17 @@ RCLONE="/home/cavedemo/W3DClassSync/rclone_bin/rclone --delete-after"
 
 LOCAL_PRE="/share/cavewriting/classes/w3d2017fall"
 
-drive_names=("Blake" "Charles" "Dani" "Griffin" "Jack" "Liesl" "Lucas" "Meredith" "Michael" "Olivia" "Sally" "Scott" "Theadora" "Tim" "Zhean")
+if [$1]
+    drive_names="$1"
+else
+    drive_names=("Blake" "Charles" "Dani" "Griffin" "Jack" "Liesl" "Lucas" "Meredith" "Michael" "Olivia" "Sally" "Scott" "Theadora" "Tim" "Zhean")
+fi
 
 #local_names=("hequet" "perez" "solayappan" "shin" "douglas" "han" "francois" "tachihara" "giannazzo" "bayer" "burke" "swanson" "vann" "dingsun" "ziebell")
 
 for i in "${!drive_names[@]}"; do
     $RCLONE sync "W3D:w3d2017Fall_Shared/${drive_names[$i]}/kioskButton" "$LOCAL_PRE/${drive_names[$i]}"
+    python3 /home/cavedemo/remove_double.py -rv "/share/cavewriting/classes/w3d2017fall/${drive_names[$i]}"
 done
 
 $RCLONE copy W3D:w3d2017Fall_Shared/Fonts /share/cavewriting/CW2/fonts
